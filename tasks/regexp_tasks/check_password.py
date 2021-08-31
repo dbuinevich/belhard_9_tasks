9257f6d22749721d607dc063c1892bf8e0001d00
Darya Buinevich <buinevich.darya@gmail.com>
2021-09-01 00:45:09 +0300

    * all tasks

--- tasks/regexp_tasks/check_password.py
+++ tasks/regexp_tasks/check_password.py
@@ -10,3 +10,21 @@
 Подсказка:
 Понадобится позитивный просмотр вперед (?=чтото)
 """
+import re
+
+
+def check_password(password: str):
+    pattern = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{8,40}$"
+    if re.fullmatch(pattern, password):
+        print('Valid password')
+    else:
+        print('Invalid password')
+
+
+if __name__ == '__main__':
+    check_password('Hhello123@t')
+    check_password('ghvgkvkgvkvvlvlv')
+    check_password('1235263636')
+    check_password('@@@@@@@@@')
+    check_password('Aa1!rj')
+    check_password('hbjhbG76$kjbbhhhbughiudfvdvbsbH!bjhbjhbkhmbjhvjgbgchfgxtfzxdfxtrd6t7gygytftyftyfyjtdf')
